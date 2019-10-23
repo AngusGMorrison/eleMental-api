31f33590c007a665b0eec0e1fdc739ee3df4c3fd
Angus Morrison <contact@angus-morrison.com>
2019-10-23 17:13:28 +0100

    Added CORS

--- controllers/hiscores.js
+++ controllers/hiscores.js
@@ -9,9 +9,7 @@ exports.create = function(req, res, next) {
   return models.Score.create({
     initials: req.body.initials,
     score: req.body.score
-  })
-  .then(score => res.json(score))
-  .catch(console.error);
+  }).then(score => res.json(score))
 }
 
 exports.test = function(req, res, next) {
