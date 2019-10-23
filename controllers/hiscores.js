2b0aaff8749e10ecf77dc1f1bb323fcf94bf600e
Angus Morrison <contact@angus-morrison.com>
2019-10-23 09:33:36 +0100

    Implemented get route for top 10 hiscores

--- controllers/hiscores.js
+++ controllers/hiscores.js
@@ -1,14 +1,19 @@
 const models = require("../models");
 
 exports.index = function(req, res, next) {
-  res.render("index", { title: "Hiscores" });
+  return models.Score.findAll({ limit: 10, order: [["score", "DESC"]] })
+    .then(scores => res.json(scores));
 }
 
 exports.create = function(req, res, next) {
   return models.Score.create({
     initials: req.body.initials,
     score: req.body.score
-  }).then(score => {
-    res.json(score);
-  });
+  })
+  .then(score => res.json(score))
+  .catch(console.error);
+}
+
+exports.test = function(req, res, next) {
+  res.render("index");
 }
