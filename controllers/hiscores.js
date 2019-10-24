8ddea3b5ab48acfe2a6419b87a39ea8dd5402201
Angus Morrison <contact@angus-morrison.com>
2019-10-24 11:26:29 +0100

    Refactored multiline arrow functions in hiscores controller

--- controllers/hiscores.js
+++ controllers/hiscores.js
@@ -8,12 +8,10 @@ index = function(req, res, next) {
 create = function(req, res, next) {
   return models.Score.create({
     initials: req.body.initials,
-    score: Number(req.body.score)
-  }).then(() => {
-    return models.Score.findTop10()
-  }).then(scores => {
-    res.json(scores);
-  });
+    score: req.body.score
+  })
+  .then(() => models.Score.findTop10())
+  .then(scores => res.json(scores));
 }
 
 module.exports = {
