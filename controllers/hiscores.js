44709b7f73979233315069ddb47ad850cf018462
Mike Thurgood <mikecthurgood@gmail.com>
2019-10-29 15:36:57 +0000

    minor refactors

--- controllers/hiscores.js
+++ controllers/hiscores.js
@@ -1,17 +1,17 @@
 const models = require("../models");
 
-index = function(req, res, next) {
+index = function (req, res, next) {
   return models.Score.findTop10()
     .then(scores => res.json(scores));
 }
 
-create = function(req, res, next) {
+create = function (req, res, next) {
   return models.Score.create({
     initials: req.body.initials,
     score: req.body.score
   })
-  .then(() => models.Score.findTop10())
-  .then(scores => res.json(scores));
+    .then(() => models.Score.findTop10())
+    .then(scores => res.json(scores));
 }
 
 module.exports = {
